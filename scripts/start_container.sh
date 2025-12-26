6c4e0a2a9aefbfd36a397dda8075dadc6d2de185
Uday <udaygandham007@gmail.com>
2025-12-26 19:27:34 +0530

    modified index.js file

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,6 +3,8 @@
 IMAGE_TAG=$(cat /home/ubuntu/deploy/tag.txt)
 echo "IMAGE_TAG=$IMAGE_TAG"
 
+aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 060152177661.dkr.ecr.ap-northeast-1.amazonaws.com
+
 docker pull 060152177661.dkr.ecr.ap-northeast-1.amazonaws.com/sample-web-app:$IMAGE_TAG
 
 docker stop web-app || true
